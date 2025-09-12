6feb071394648c168b4137974fbffcb87887a0ea
Ajit Singh <ajit1608@gmail.com>
2025-09-12 13:32:58 +0800

    Class Notes Day 3 12-Sep-2025

--- src/string/HelloWorld.java
+++ src/string/HelloWorld.java
@@ -1,17 +1,18 @@
 /**
- * 
+ * default package for string classes
  */
 package string;
 
 /**
- * 
+ * Class to Print "Hello World!!!"
  */
 public class HelloWorld {
 
 	/**
-	 * @param args
+	 * Parameter argument
+	 * @param args 
 	 */
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 		System.out.println("Hello World");
 
 	}
